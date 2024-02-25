0f2b2e5fe9c1498735b0e7b25ad5aaa8f8ec767e
florinhegedus <florinxhgds@gmail.com>
2024-02-25 19:31:49 +0200

    adaptive density control

--- gaussian.py
+++ gaussian.py
@@ -11,7 +11,7 @@ init_logging()
 
 
 class GaussianModel(nn.Module):
-    def __init__(self, mri_data, k_sigma, k_intensity, tau, max_intensity=None, device='cpu') -> None:
+    def __init__(self, mri_data, k_sigma, k_intensity, tau, N_max=5000, max_intensity=None, device='cpu') -> None:
         super(GaussianModel, self).__init__()
 
         self.device = device
@@ -29,10 +29,15 @@ class GaussianModel(nn.Module):
             torch.linspace(0, 1, steps=mri_data.shape[2], device=self.device)
         ], indexing='ij'), dim=-1)  # Shape: [X, Y, Z, 3]
 
+        self.centers = nn.Parameter(torch.zeros(N_max, 3).requires_grad_(True)).to(self.device)
+        self.sigmas = nn.Parameter(torch.zeros(N_max).requires_grad_(True)).to(self.device)
+        self.intensities = nn.Parameter(torch.zeros(N_max).requires_grad_(True)).to(self.device)
+
         centers, sigmas, intensities = self.setup_functions(k_sigma, k_intensity, tau)
-        self.centers = nn.Parameter(centers.requires_grad_(True)).to(self.device)
-        self.sigmas = nn.Parameter(sigmas.requires_grad_(True)).to(self.device)
-        self.intensities = nn.Parameter(intensities.requires_grad_(True)).to(self.device)
+
+        self.centers.data[:centers.size(0), :] = centers
+        self.sigmas.data[:sigmas.size(0)] = sigmas
+        self.intensities.data[:intensities.size(0)] = intensities
 
     def setup_functions(self, k_sigma, k_intensity, tau):
         logging.info("Initializing Gaussians")
@@ -73,7 +78,7 @@ class GaussianModel(nn.Module):
         # Find Gaussian Centers Based on Gradient Magnitude
         logging.info("Select points with gradient magnitude around the median as centers")
         median_grad = np.median(filtered_grad_magnitude[filtered_grad_magnitude > 0])
-        close_to_median = np.abs(filtered_grad_magnitude - median_grad) < (0.01 * median_grad)
+        close_to_median = np.abs(filtered_grad_magnitude - median_grad) < (0.02 * median_grad)
         center_idxs = np.argwhere(close_to_median)
         centers_tensor = torch.tensor(center_idxs, dtype=torch.long, device=self.device)
 
@@ -104,8 +109,9 @@ class GaussianModel(nn.Module):
     
     def forward(self):
         volume = torch.zeros(self.volume_shape, dtype=torch.float, device=self.device)
+        num_active_gaussians = self.sigmas[self.sigmas > 0].size(0)
 
-        for i in range(len(self.centers)):
+        for i in range(num_active_gaussians):
             center_norm = self.centers[i]  # Normalized center
             sigma = self.sigmas[i]
             intensity = self.intensities[i]
@@ -142,6 +148,106 @@ class GaussianModel(nn.Module):
             volume[min_x:max_x, min_y:max_y, min_z:max_z] += contribution
 
         return volume
+    
+    def clone_gaussian(self, index, centers, sigmas, intensities):
+        center = centers[index].clone().detach()
+        sigma = sigmas[index].clone().detach()
+        intensity = intensities[index].clone().detach() / 2
+
+        grad_direction = self.centers.grad[index] / self.centers.grad[index].norm()
+        new_center = center + grad_direction * 0.01
+
+        # Correctly wrap the updated tensors with nn.Parameter
+        centers = torch.cat((centers, new_center.unsqueeze(0)), dim=0)
+        sigmas = torch.cat((sigmas, sigma.unsqueeze(0)), dim=0)
+        intensities = torch.cat((intensities, intensity.unsqueeze(0)), dim=0)
+
+        return centers, sigmas, intensities
+
+    def split_gaussian(self, index, centers, sigmas, intensities, phi=1.6):
+        center = centers[index]
+        sigma = sigmas[index] / phi  # Reduce sigma by a factor phi
+        intensity = intensities[index] / 2  # Halve the intensity for both splits
+        
+        # Direction for splitting can be based on some heuristic or random variation
+        split_direction = torch.randn_like(center)
+        split_direction /= split_direction.norm()
+        
+        # Initialize positions of the new Gaussians by slightly offsetting them from the original
+        new_center1 = center + split_direction * sigma
+        new_center2 = center - split_direction * sigma
+        
+        # Remove the original Gaussian and add the new ones
+        centers = torch.cat((centers[:index], centers[index+1:], new_center1.unsqueeze(0), new_center2.unsqueeze(0)), dim=0)
+        sigmas = torch.cat((sigmas[:index], sigmas[index+1:], sigma.unsqueeze(0), sigma.unsqueeze(0)), dim=0)
+        intensities = torch.cat((intensities[:index], intensities[index+1:], intensity.unsqueeze(0), intensity.unsqueeze(0)), dim=0)
+
+        return centers, sigmas, intensities
+    
+    def prune_gaussians(self, epsilon_alpha, centers, sigmas, intensities):
+        # Identify Gaussians to prune based on intensity threshold
+        mask = intensities > epsilon_alpha
+        
+        # Remove identified Gaussians
+        centers = centers[mask]
+        sigmas = sigmas[mask]
+        intensities = intensities[mask]
+
+        return centers, sigmas, intensities
+
+    def adaptive_density_control(self, tau_pos=0.0001, epsilon_alpha=0.01, phi=1.5):
+        """
+        Adjusts the density of Gaussians based on reconstruction needs.
+
+        Args:
+        - tau_pos (float): Threshold for positional gradient magnitude to trigger densification.
+        - epsilon_alpha (float): Threshold below which Gaussians are considered for pruning.
+        - phi (float): Division factor for the sigma of split Gaussians.
+        """
+        logging.info("Running adaptive density control")
+        # Ensure gradients are computed
+        if self.centers.grad is None:
+            return
+
+        # Calculate the norm of the positional gradients
+        grad_norms = self.centers.grad.norm(dim=1)
+
+        # Get active gaussians
+        num_active_gaussians = self.sigmas[self.sigmas > 0].size(0)
+
+        # Get the values for centers, sigmas and intensities
+        centers = self.centers.data[:num_active_gaussians]
+        sigmas = self.sigmas.data[:num_active_gaussians]
+        intensities = self.intensities.data[:num_active_gaussians]
+
+        splitted = 0
+        cloned = 0
+        idx = 0
+
+        while idx < num_active_gaussians:
+            # Determine action based on gradient magnitude and Gaussian size
+            if grad_norms[idx] > tau_pos:                # For large Gaussians or those in over-reconstructed areas, consider splitting
+                if sigmas[idx] > (sigmas.mean() * phi):
+                    splitted += 1
+                    centers, sigmas, intensities = self.split_gaussian(idx, centers, sigmas, intensities, phi)
+                    continue
+                elif self.sigmas[idx] < (self.sigmas.mean() / phi):
+                    # For small Gaussians or under-reconstructed areas, consider cloning
+                    cloned += 1
+                    centers, sigmas, intensities = self.clone_gaussian(idx, centers, sigmas, intensities)
+            idx += 1
+
+        no_before_pruning = sigmas.size(0)
+        centers, sigmas, intensities = self.prune_gaussians(epsilon_alpha, centers, sigmas, intensities)
+
+        logging.info(f"Number of gaussians splitted={splitted}")
+        logging.info(f"Number of gaussians cloned={cloned}")
+        logging.info(f"Number of gaussians pruned={no_before_pruning - sigmas.size(0)}")
+        logging.info(f"Total number of gaussians: {len(centers)}")
+
+        self.centers.data[:centers.size(0), :] = centers
+        self.sigmas.data[:sigmas.size(0)] = sigmas
+        self.intensities.data[:intensities.size(0)] = intensities
 
     def loss(self, reconstructed_volume):
         # Implement the loss function. For example, use Mean Squared Error (MSE) between the reconstructed volume and the original MRI data
@@ -150,7 +256,7 @@ class GaussianModel(nn.Module):
 
 
 def main():
-    lr_mri_path = '../../hcp1200/996782/T1w_acpc_dc_restore_brain_downsample_factor_2.nii.gz'
+    lr_mri_path = '../../hcp1200/996782/T1w_acpc_dc_restore_brain_downsample_factor_4.nii.gz'
     lr_mri = MRI.from_nii_file(lr_mri_path)
 
     device = get_device()
@@ -158,9 +264,12 @@ def main():
     k_sigma = 0.4
     k_intensity = 0.4 
     tau = 0.05 # threshold for excluding empty spaces
-    max_intensity = 1 # divide intensities by this value
+    N_max = 5000 # max number of gaussians
+    max_intensity = 1 # divide intensities by this value, None for dividing with max(intensities)
+    adaptive_density_control = True
+    densify_frequency = 10
 
-    gm = GaussianModel(lr_mri.data, k_sigma, k_intensity, tau, max_intensity)
+    gm = GaussianModel(lr_mri.data, k_sigma, k_intensity, tau, N_max, max_intensity)
     
     # Separate parameters into groups for different learning rates
     center_params = [gm.centers]
@@ -173,7 +282,7 @@ def main():
     ], betas=(0.9, 0.999))
 
     # Define the number of epochs or iterations for optimization
-    num_epochs = 50
+    num_epochs = 100
 
     # Define an exponential decay for the learning rate of centers
     lambda1 = lambda epoch: (2e-6 / 2e-4) ** (epoch / num_epochs)
@@ -191,8 +300,12 @@ def main():
                 print(f"Gradient for {name}: {param.grad.norm().item()}")
             else:
                 print(f"No gradient for {name}")
+
         optimizer.step()  # Update parameters
         scheduler.step()
+
+        if epoch % densify_frequency == densify_frequency - 1 and adaptive_density_control and epoch < num_epochs / 2:
+            gm.adaptive_density_control()
 
         logging.info(f"Epoch {epoch}, Loss: {loss.item()}")
 
